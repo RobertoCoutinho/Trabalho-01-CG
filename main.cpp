0404661c28155331eefcdcccc8f793f82941d7c6
RobertoCoutinho <robertoo.coutinho2014@gmail.com>
2024-08-31 22:58:51 -0300

    initial commit

--- main.cpp
+++ main.cpp
@@ -30,6 +30,9 @@ float up_x = 0;
 float up_y = 1;
 float up_z = 0;
 
+int direcao_translacao = 1;
+bool define_camera_livre = false;
+
 void defineCamera1(){
     centro_x = 3.5;
     centro_y = 14;
@@ -43,20 +46,23 @@ void defineCamera1(){
     up_y = 1;
     up_z = 0;
 }
+
 void defineCamera2(){
     centro_x = 3.5;
     centro_y = 14;
-    centro_z = -20;
+    centro_z = -17;
 
     olho_x = 3.5;
     olho_y = 4;
-    olho_z = 0;
+    olho_z = -7;
 
     up_x = 0;
     up_y = 1;
     up_z = 0;
+
 }
 
+/*
 Vetor3D transformedPoint(Vetor3D p)
 {
     glMatrixMode(GL_MODELVIEW);
@@ -78,12 +84,10 @@ Vetor3D transformedPoint(Vetor3D p)
 
     Vetor3D res3D = Vetor3D(res[0],res[1],res[2]);
     return res3D;
-}
+}*/
 
 void displayInner() {
     GUI::setLight(6,3.5,8,-3.5,true,false);
-    GUI::setLight(6,3.5,8,-8,true,false);
-    GUI::setLight(6,3.5,8,3,true,false);
 
     glPushMatrix();
     tabuleiro->desenha();
@@ -249,76 +253,26 @@ void desenha() {
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
 
-        /*
-        gluLookAt(
-            glutGUI::cam->e.x,
-            glutGUI::cam->e.y,
-            glutGUI::cam->e.z,
-            glutGUI::cam->c.x,
-            glutGUI::cam->c.y,
-            glutGUI::cam->c.z,
-            glutGUI::cam->u.x,
-            glutGUI::cam->u.y,
-            glutGUI::cam->u.z);
-        */
-
-        gluLookAt(
-            centro_x,centro_y,centro_z,
-            olho_x, olho_y, olho_z,
-            up_x,up_y,up_z);
+        if(define_camera_livre){
+            gluLookAt(
+                glutGUI::cam->e.x, glutGUI::cam->e.y, glutGUI::cam->e.z,
+                glutGUI::cam->c.x, glutGUI::cam->c.y, glutGUI::cam->c.z,
+                glutGUI::cam->u.x, glutGUI::cam->u.y, glutGUI::cam->u.z);
+        }
+        else{
+            gluLookAt(
+                centro_x,centro_y,centro_z,
+                olho_x, olho_y, olho_z,
+                up_x, up_y, up_z);
+        }
 
         displayInner();
 
-        if (!objetos.empty()) {
-            glLoadIdentity();
-            glViewport(0, 3*glutGUI::height/4, glutGUI::width/4, glutGUI::height/4);
-            Vetor3D zlocalemCoordsGlobais = transformedPoint(Vetor3D(0,0,1));
-            Vetor3D olho = objetos[0]->t + zlocalemCoordsGlobais*1.2;
-            Vetor3D centro = olho + zlocalemCoordsGlobais;
-
-            Vetor3D ylocalemCoordsGlobais = transformedPoint(Vetor3D(0,1,0));
-            Vetor3D up = ylocalemCoordsGlobais;
-
-            gluLookAt(olho.x,olho.y,olho.z, centro.x,centro.y,centro.z, up.x,up.y,up.z);
-            displayInner();
-        }
-
-
         if (posSelecionado >= 0 and posSelecionado < objetos.size()) {
-            objetos[posSelecionado]->t.x += 55 * glutGUI::dtx;
-            objetos[posSelecionado]->t.y += 55 * glutGUI::dty;
-            objetos[posSelecionado]->t.z += 55 * glutGUI::dtz;
-            /*
-            objetos[posSelecionado]->a.x += glutGUI::dax;
-            objetos[posSelecionado]->a.y += glutGUI::day;
-            objetos[posSelecionado]->a.z += glutGUI::daz;
-
-            objetos[posSelecionado]->s.x += glutGUI::dsx;
-            objetos[posSelecionado]->s.y += glutGUI::dsy;
-            objetos[posSelecionado]->s.z += glutGUI::dsz;
-            */
+            objetos[posSelecionado]->t.x += 55 * (direcao_translacao * glutGUI::dtx);
+            objetos[posSelecionado]->t.y += 55 * (direcao_translacao * glutGUI::dty);
+            objetos[posSelecionado]->t.z += 55 * (direcao_translacao * glutGUI::dtz);
         }
-    //    glutGUI::dtx = 0.0; glutGUI::dty = 0.0; glutGUI::dtz = 0.0;
-    //    glutGUI::dax = 0.0; glutGUI::day = 0.0; glutGUI::daz = 0.0;
-    //    glutGUI::dsx = 0.0; glutGUI::dsy = 0.0; glutGUI::dsz = 0.0;
-
-        //objeto transformado
-        //glPushMatrix();
-    //        glTranslatef(glutGUI::tx,glutGUI::ty,glutGUI::tz);
-    //        glRotatef(glutGUI::az,0,0,1);
-    //        glRotatef(glutGUI::ay,0,1,0);
-    //        glRotatef(glutGUI::ax,1,0,0);
-    //        GUI::drawOrigin(1);
-    //        glScalef(glutGUI::sx,glutGUI::sy,glutGUI::sz);
-    //        GUI::setColor(0,0,1);
-    //        //GUI::drawBox(0,0,0, 1,1,1);
-    //        //GUI::drawBox(1,1,0, 2,2,1);
-    //        //casa();
-    //        //personagem();
-    //        //GUI::draw3ds(model3ds);
-
-        //glPopMatrix();
-
         GUI::displayEnd();
     }
 }
@@ -337,6 +291,7 @@ void teclado(unsigned char key, int x, int y) {
             objetos[posSelecionado]->selecionado = false;
         }
         posSelecionado = -1;
+        direcao_translacao = 1;
         break;
     case '2':
         defineCamera2();
@@ -344,6 +299,11 @@ void teclado(unsigned char key, int x, int y) {
             objetos[posSelecionado]->selecionado = false;
         }
         posSelecionado = 16;
+        direcao_translacao = -1;
+        break;
+    case 'c':
+        define_camera_livre = !define_camera_livre;
+        glutGUI::trans_obj = !glutGUI::trans_obj;
         break;
     case 't':
         glutGUI::trans_obj = !glutGUI::trans_obj;
@@ -374,67 +334,6 @@ void teclado(unsigned char key, int x, int y) {
             objetos[posSelecionado]->selecionado = true;
         }
         break;
-
-
-    case 'O':
-        incluirObjeto = !incluirObjeto;
-        break;
-    case 'p':
-        if (incluirObjeto) {
-            //objetos.push_back( new Personagem() );
-        }
-        break;
-    case 'x':
-        if (incluirObjeto) {
-            objetos.push_back( new Peao(1) );
-        }
-        break;
-    case 'P':
-        pause = !pause;
-        break;
-    case 'c':
-        if (incluirObjeto) {
-            //objetos.push_back( new Carro() );
-        }
-        break;
-    case 'C':
-        if (incluirObjeto) {
-            //objetos.push_back( new Casa() );
-        }
-        break;
-    case '4':
-        if (incluirObjeto) {
-            //objetos.push_back( new novoObjeto() );
-        }
-        break;
-//    case 'i':
-//        //glutGUI::tx = 0.0;
-//        //glutGUI::ty = 0.0;
-//        //glutGUI::tz = 0.0;
-//        //glutGUI::ax = 0.0;
-//        //glutGUI::ay = 0.0;
-//        //glutGUI::az = 0.0;
-//        //glutGUI::sx = 1.0;
-//        //glutGUI::sy = 1.0;
-//        //glutGUI::sz = 1.0;
-//        //glutGUI::lx = 0.0;
-//        //glutGUI::ly = 0.0;
-//        //glutGUI::lz = 0.0;
-//        if (posSelecionado >= 0 and posSelecionado < objetos.size()) {
-//            objetos[posSelecionado]->t.x = 0.0;
-//            objetos[posSelecionado]->t.y = 0.0;
-//            objetos[posSelecionado]->t.z = 0.0;
-
-//            objetos[posSelecionado]->a.x += glutGUI::dax;
-//            objetos[posSelecionado]->a.y += glutGUI::day;
-//            objetos[posSelecionado]->a.z += glutGUI::daz;
-
-//            objetos[posSelecionado]->s.x += glutGUI::dsx;
-//            objetos[posSelecionado]->s.y += glutGUI::dsy;
-//            objetos[posSelecionado]->s.z += glutGUI::dsz;
-//        }
-//        break;
-
     default:
         break;
     }
@@ -486,6 +385,7 @@ void iniciarPecas(){
 
 int main()
 {
+    glutGUI::trans_obj = true;
     iniciarPecas();
     cout << "Hello World!" << endl;
 
